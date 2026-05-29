c49da6490f2cea84d0f463a67d0ab25e25e67356
Benjamín Silva <benjamin@nexoit.com>
2026-05-29 16:05:52 -0400

    chore(android): bump version 1.1.0 code 4

--- src/utils/appVersion.ts
+++ src/utils/appVersion.ts
@@ -1,4 +1,4 @@
 // Auto-update these values on each release.
 export const APP_VERSION = '1.1.0';
-export const APP_BUILD = 3;
+export const APP_BUILD = 4;
 export const APP_VERSION_LABEL = `v${APP_VERSION} (${APP_BUILD})`;
