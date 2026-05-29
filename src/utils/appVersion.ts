e69d746371cbf8884b7e6a9057158399bc780638
Benjamín Silva <benjamin@nexoit.com>
2026-05-29 15:58:10 -0400

    chore(ios): bump version 1.1.0 build 3

--- src/utils/appVersion.ts
+++ src/utils/appVersion.ts
@@ -1,4 +1,4 @@
 // Auto-update these values on each release.
-export const APP_VERSION = '1.0.0';
-export const APP_BUILD = 1;
+export const APP_VERSION = '1.1.0';
+export const APP_BUILD = 3;
 export const APP_VERSION_LABEL = `v${APP_VERSION} (${APP_BUILD})`;
