4eca463e731e6f3cb9fa1412b2a8421938641cc2
Benjamín Silva <benjamin@nexoit.com>
2026-07-28 22:39:29 -0400

    chore(android): bump version 1.1.2 code 6

--- src/utils/appVersion.ts
+++ src/utils/appVersion.ts
@@ -1,4 +1,4 @@
 // Auto-update these values on each release.
-export const APP_VERSION = '1.1.1';
-export const APP_BUILD = 5;
+export const APP_VERSION = '1.1.2';
+export const APP_BUILD = 6;
 export const APP_VERSION_LABEL = `v${APP_VERSION} (${APP_BUILD})`;
